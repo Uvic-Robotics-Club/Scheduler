e5d7f346f254cff9de4b3b89a9d9e413364b752e
Andrew Rose <andrew.rose43@gmail.com>
2018-07-14 14:58:17 -0700

    Edited the Vector3 constructor

--- shelltypes.py
+++ shelltypes.py
@@ -2,10 +2,10 @@
 #Also has a handy print method!
 class Vector3:
 
-    def __init__(self, ex=0, why=0, zed=0):
-        self.x = ex
-        self.y = why
-        self.z = zed
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
 
     def __str__(self):
         return "x = {}\ny = {}\nz = {}\n\n".format(self.x,self.y,self.z)
