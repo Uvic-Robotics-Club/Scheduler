b76b8e886be08b5afeb50bde232fbab7292a124b
Gregory <gregoryrohagan@gmail.com>
2019-09-26 18:37:48 -0700

    Added simple consumers and producers for tutorials

---
+++ sample_material.py
@@ -0,0 +1,102 @@
+from scheduler_types import Pq_obj
+
+from time import time, sleep
+
+#Simple object for testing Scheduler. Uses a polled timer to add a task to the queue, which then prints
+class Self_contained_timer:
+	def __init__(self, val):
+		self.val = val
+		self.time = time()
+
+	# Ensures this doesn't happen more often than every val seconds
+	def poll_function(self):
+		if time() - self.time > self.val:
+			self.time = time()
+			#3 is the priority of this event
+			return [Pq_obj(3, self.event_function)]
+
+	# Checks that functions can be returned by polled functions
+	def event_function(self):
+		print("Event function " + str(self.val) + " is running on a thread")
+		return [Pq_obj(3, self.do_later_function, self.val + 1)]
+
+	# checks that functions can be returned by functions in queue
+	def do_later_function(self, num):
+		print("This ran a bit later from event function " + str(num - 1))
+
+
+class Simple_producer:
+	def __init__(self):
+		self.time = time()
+		self.number = 0
+		self.functs = []
+
+	# Run every 1 second
+	def poll_function(self):
+		if time() - self.time > 1:
+			# Note that this implementation actually runs once per second, rather than the
+			# approximate implementation in Self_contained_timer
+			self.time += 1
+			# 2 is the priority of this event - a higher priority than the average of '3'
+			return [Pq_obj(3, self.event_function)]
+
+	# When poll function determines something should happen, this is where it actually happens
+	# It's okay to take a bit of time here, but do not block.
+	def event_function(self):
+		# Do any internal work
+		self.number += 1
+
+		# Get ready all the functions that are supposed to run on an event.
+		# We won't actually run them here - we'll give them back to the scheduler to run when
+		# it is ready to
+		todo = []
+		if self.functs:
+			for f in self.functs:
+				todo.append(Pq_obj(3, f, self.number))
+
+		return todo
+
+	# Provides a way to link functions from other objects to events from this producer.
+	# Note: it is the consumer's responsibility to
+	def add_function_to_call(self, function):
+		self.functs.append(function)
+
+
+class Simple_middleman:
+	def __init__(self):
+		# list of functions this class will call
+		self.functs = []
+
+	# When poll function determines something should happen, this is where it actually happens
+	# It's okay to take a bit of time here, but do not block.
+	def on_event(self, number):
+
+		# Do any internal work
+		# Note: this sleep is for demonstrational purposes only.
+		# DO NOT use sleep calls in your own code, as they will block the scheduler
+		sleep(5)
+
+		# Get ready all the functions that are supposed to run on an event.
+		# We won't actually run them here - we'll give them back to the scheduler to run when
+		# it is ready to
+		todo = []
+		if self.functs:
+			for f in self.functs:
+				todo.append(Pq_obj(3, f, self.number))
+
+		return todo
+
+
+	# Provides a way to link functions from other objects to events from this producer.
+	# Note: it is the consumer's responsibility to
+	def add_function_to_call(self, function):
+		self.functs.append(function)
+
+
+class Simple_consumer:
+	def __init__(self, modNum):
+		self.modNum = modNum
+
+	def on_event(self, number):
+		if number % self.modNum is 0:
+			print(number, 'is divisible by', self.modNum)
---
+++ scheduler_start_tutorial.py
@@ -0,0 +1,64 @@
+import scheduler
+import workAdder
+
+from scheduler_types import *
+from sample_material import *
+
+def main():
+	# List of functions for the scheduler to loop through
+	pollFuncts = []
+
+	# ----- Create everything here ----
+
+	# ----------------------------------------------------------------
+	# Part 1
+	# Create a timer and add it to the our list of poll functions
+
+	# timer = Self_contained_timer(1)
+	# pollFuncts.append(timer.poll_function)
+
+	# ----------------------------------------------------------------
+	# Part 2
+	# Create a producer, and link it two different consumers
+
+	# p = Simple_producer()
+	# pollFuncts.append(p.poll_function)
+	#
+	# c1 = Simple_consumer(2)
+	# p.add_function_to_call(c1.on_event)
+	# c2 = Simple_consumer(3)
+	# p.add_function_to_call(c2.on_event)
+
+	# ----------------------------------------------------------------
+	# Part 3
+	# one producer, a middleman, and two end consumers
+
+	# p2 = Simple_producer()
+	# pollFuncts.append(p2.poll_function)
+	#
+	# m = Simple_middleman()
+	# p2.add_function_to_call(m.on_event)
+	#
+	# c3 = Simple_consumer(2)
+	# m.add_function_to_call(c3.on_event)
+	# c4 = Simple_consumer(3)
+
+	# Note that c4.on_event is being called by two different sources.
+	# There's no restriction on who can call what as long as (a) you don't create any loops, and
+	# (b) the parameters of the producer match the consumer
+
+	m.add_function_to_call(c4.on_event)
+	p2.add_function_to_call(c4.on_event)
+
+	# ----- Below here will rarely need to change -----
+
+	# Initialize and start the program
+	# '8' is the maximum number of simultaneous threads, and 'functs' is the polling function list
+	s = scheduler.Scheduler(8, pollFuncts)
+	# Create the workAdder singleton
+	wA = workAdder.workAdder(s)
+	s.run()
+
+
+if __name__ == '__main__':
+	main()
