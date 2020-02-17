1c26c497450296dcdff196235bd11bb0efbcdb9e
Anton Nikitenko <ant.nikitenko@gmail.com>
2020-02-17 10:32:11 -0800

    Refactor improvements

--- scheduler.py
+++ scheduler.py
@@ -6,22 +6,18 @@ from scheduler_types import *
 class Scheduler:
 	# Defined constants for the operation of the `poll_loop()` target thread
 	POLL_THREAD_START_TIMEOUT_SEC = 5.00
-	POLL_THREAD_ACTIVE_POLL_INTERVAL_SEC = 0.01
 	POLL_THREAD_MINIMUM_POLL_TIME_SEC = 0.01
 
 	# Defined constants for the operation of the `event_loop()` target thread
 	EVENT_THREAD_START_TIMEOUT_SEC = 5.00
-	EVENT_THREAD_ACTIVE_POLL_INTERVAL_SEC = 0.01
 	EVENT_THREAD_BLOCKED_POLL_INTERVAL_SEC = 0.01
 
 	# Defined constants for the operation of the `funct_runner()` target thread
 	FUNCT_RUNNER_THREAD_START_TIMEOUT_SEC = 0.50
 
     # Define the minimum thread count required by the Scheduler to fulfill its purpose. The minimum thread 
-	# count is 3, where each thread is allocated as follows:
-	#     - Threads 1 & 2: Assigned for the poll loop and event loop
-	#     - Thread 3: Assigned to handle `funct_runner` targets.
-	MINIMUM_THREAD_COUNT = 3
+	# count is 1, since at least 1 worker thread is required for a task at any time.
+	MIN_WORKER_THREAD_COUNT = 1
 
 	def __init__(self, maxThreadCount, pollFunctionsList):
 		""" Construct an instance of the Scheduler class.
@@ -35,22 +31,29 @@ class Scheduler:
 		  None
 		"""
 
-		self.threadCount = 0
-		self.pollFunctionsList = pollFunctionsList
-		self.priorityQueue = queue.PriorityQueue()
-		self.lock = threading.Lock()
+		if type(maxThreadCount) != int:
+			return
 
-		if maxThreadCount >= self.MINIMUM_THREAD_COUNT:
+		for function in pollFunctionsList:
+			if callable(function) == False: 
+				return
+
+		self.threadCount = 0
+		self.threadCountLock = threading.Lock()
+		self.pollFunctionsList = pollFunctionsList
+		self.taskQueue = queue.PriorityQueue()
+		
+		if maxThreadCount >= self.MIN_WORKER_THREAD_COUNT:
 			self.maxThreadCount = maxThreadCount
 		else:
-			self.maxThreadCount = self.MINIMUM_THREAD_COUNT
+			self.maxThreadCount = self.MIN_WORKER_THREAD_COUNT
 
 		self.active = False
 
-	def start(self):
+	def run(self):
 		""" Starts the instance of the Scheduler class, by starting the two necessary threads targetting
 		`poll_loop()` and `event_loop()` respectively. If any of the two threads fail to start, then exit
-		the scheduler. If the method completes successfully, sets the state of the scheduler to Active.
+		the scheduler.
 
 		Args:
 		  None
@@ -58,33 +61,27 @@ class Scheduler:
 		- (bool) - Returns only `False` if any of the two threads failed to start, otherwise `None`
 		"""
 
-		print("Starting the scheduler")
+		print("[INFO]: Starting the scheduler")
 
-        # Starts the thread with `poll_loop()` as the target. Increments the thread count if successful
+		self.active = True
+
+        # Starts the thread with `poll_loop()` as the target
 		startPollLoopEvent = threading.Event()
 		self.pollLoopThread = threading.Thread(target=self.poll_loop, args=(startPollLoopEvent,))
 		self.pollLoopThread.start()
+
 		if startPollLoopEvent.wait(timeout=self.POLL_THREAD_START_TIMEOUT_SEC) != True:
 			print("[ERROR]: Failed to start Poll loop thread - Exiting scheduler.")
-			# TODO: Raise Exception? May be used to handle failure case
+			self.active = False
 			return False
 		else:
 			print("[INFO]: Poll loop thread started.")
-			self.incrementThreadCount()
 
 		# Starts the thread with `event_loop` as the target. Increments the thread count if successful
 		startEventLoopEvent = threading.Event()
 		self.eventLoopThread = threading.Thread(target=self.event_loop, args=(startEventLoopEvent,))
 		self.eventLoopThread.start()
-		if startEventLoopEvent.wait(timeout=self.EVENT_THREAD_START_TIMEOUT_SEC) != True:
-			print("[ERROR]: Failed to start Event loop thread - Exiting scheduler.")
-			# TODO: Raise Exception? May be used to handle failure case
-			return False
-		else:
-			print("[INFO]: Event loop thread started.")
-			self.incrementThreadCount()
-
-		self.active = True
+		print("[INFO]: Event loop thread started.")
 
 	def stop(self):
 		""" Stops the instance of the Scheduler class in such a way that it can be restarted with a subsequent
@@ -102,17 +99,15 @@ class Scheduler:
 		print("[INFO]: Stopping the scheduler...")
 
 		self.pollLoopThread.join()
-		self.decrementThreadCount()
 		print("[INFO]: Poll loop thread terminated.")
 
 		self.eventLoopThread.join()
-		self.decrementThreadCount()
 		print("[INFO]: Event loop thread terminated.")
 
 		# Check for any tasks in the priority queue that have not been processed by the event loop
-		if not self.priorityQueue.empty():
+		if not self.taskQueue.empty():
 			print("[WARNING]: Priority queue is not empty. Emptying Priority queue.")
-			self.priorityQueue = queue.PriorityQueue()
+			self.taskQueue = queue.PriorityQueue()
 
 		return True
 
@@ -129,31 +124,27 @@ class Scheduler:
 		  None 
 		"""
 
-		startPollLoopEvent.set()
-
-		# Blocks until the scheduler is set to active
-		while not self.active:
-			sleep(self.POLL_THREAD_ACTIVE_POLL_INTERVAL_SEC)
-
 		# Close this thread if there aren't any poll functions stored in the list
 		if len(self.pollFunctionsList) == 0:
 			print("[ERROR]: No poll functions - closing thread.")
 			return
 
+		startPollLoopEvent.set()
+
 		# Loop through all polling functions
+		# TODO: Investigate and implement a more precise timing mechanism
+		# TODO: Replace sleep(self.minPollTime - (currTime -prevTime)) with timer.wait ?
 		timer = threading.Event()
 		while self.active and not timer.wait(self.POLL_THREAD_MINIMUM_POLL_TIME_SEC):
 			for function in self.pollFunctionsList:
 				result = function()
-				if result:
-					for pq_obj in result:
-						self.priorityQueue.put(pq_obj)
+				self.pushQueueTasks(result)
 
 	# TODO: add some way to kill a thread that takes too long
 	# Handles tasks from priority queue
 	def event_loop(self, startEventLoopEvent):
 		""" The target of the event function thread. Blocks until the Scheduler is active, and continuously 
-		attempts to get the next task off `priorityQueue`. A thread is then started for each task, with
+		attempts to get the next task off `taskQueue`. A thread is then started for each task, with
 		`funct_runner()` as the target.
 
 		Args:
@@ -163,18 +154,12 @@ class Scheduler:
 		  None
 		"""
 
-		startEventLoopEvent.set()
-
-		# Blocks until the scheduler is set to active
-		while not self.active:
-			sleep(self.EVENT_THREAD_ACTIVE_POLL_INTERVAL_SEC)
-
 		while self.active:
-			if self.threadCount < self.maxThreadCount and self.priorityQueue.qsize() > 0:
-				# Since queue.PriorityQueue.gsize() does not guarantee that a subsequent call to get()
+			if self.threadCount < self.maxThreadCount and self.taskQueue.qsize() > 0:
+				# Since queue.PriorityQueue.qsize() does not guarantee that a subsequent call to get()
 				# will not block, necessary to handle the failure case.
 				try:
-					obj = self.priorityQueue.get(block=False)
+					obj = self.taskQueue.get(block=False)
 				except queue.Empty:
 					print("[WARNING]: Attempted to get task from empty Priority Queue")
 					continue
@@ -211,16 +196,35 @@ class Scheduler:
 
 		"""
 
+		# TODO: Move to end of function, where indicating success
 		startFunctionRunnerEvent.set()
 
-		res = None
+		result = None
 		if args:
-			res = target(args)
+			result = target(args)
 		else:
-			res = target()
-		if res:
-			for funct in res:
-				self.priorityQueue.put(funct)
+			result = target()
+
+		self.pushQueueTasks(result)
+
+	def pushQueueTasks(self, taskList):
+		
+		if taskList == None:
+			return True
+		elif type(taskList) == list:
+			for task in taskList:
+				if type(task) == Pq_obj:
+					self.taskQueue.put(task)
+				else:
+					print("[ERROR]: Failed to add object, wrong type!")
+					return False
+		elif type(taskList) == Pq_obj:
+			self.taskQueue.put(taskList)
+		else:
+			print("[ERROR]: Failed to add task, wrong type!")
+			return False
+
+		return True
 				
 	def incrementThreadCount(self):
 		""" A thread-safe wrapper to the local `threadCount` variable, allowing it 
@@ -232,7 +236,7 @@ class Scheduler:
 		  (bool) - Returns only True when the local `threadCount` variable has been incremented
 		"""
 
-		with self.lock:
+		with self.threadCountLock:
 			self.threadCount += 1
 		return True
 
@@ -246,7 +250,7 @@ class Scheduler:
 		  (bool) - Returns only True when the local `threadCount` variable has been decremented
 		"""
 
-		with self.lock:
+		with self.threadCountLock:
 			self.threadCount -= 1
 		return True
 
@@ -290,7 +294,7 @@ def main():
 	functions.append(obj.poll_function)
 
 	scheduler = Scheduler(8, functions)
-	scheduler.start()			# Start both loops
+	scheduler.run()			# Start both loops
 	sleep(6)
 	scheduler.stop()
 
