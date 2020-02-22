e6072125af0e7340c143ec0cdfeb07c6e674f803
BaraaDawod <baraa.h.dawod@gmail.com>
2020-02-22 12:25:36 -0800

    First commit with changes to joystic_demo.py and arduino_ports_init.py
    test on raspberry pi to make sure ports are detected correctly

--- arduino_ports_init.py
+++ arduino_ports_init.py
@@ -11,36 +11,98 @@ class Arduino_serial_finder:
 	# dictionary will hold all the serial port information of the arduinos connected to the
 	# raspberry pi after they are registered when the ports are scanned.
 	def __init__(self):
-		self.book = {}
+		self.COM_ports = {}
 
 
 
 	# This is to identify the arduino based on its 'ID' and returns its serial information
 	# if registered
-	def get_serial(self, arduino_id):
-		for i in self.book:
-			print(i)
-			if arduino_id in i:
-				return self.book[i]
+	def get_serial_port(self, arduino_id):
+		for port in self.COM_ports:
+			print(port)
+			if arduino_id in port:
+				return self.COM_ports[port]
 		return None
 
+
+	def read_from_serial(self, ser, BAUD_RATE):
+		# the messages sent and received are contained in brackets
+		PACKET_START_MARKER = '>'
+		PACKET_END_MARKER = '<'
+
+		# signal read is stored in the variabe read_input
+		read_input = ''
+		reading_in_progress = True
+
+		while reading_in_progress:
+
+			# read from serial
+			tmp_char = ser.read().decode('utf-8')
+			if(tmp_char == PACKET_START_MARKER):
+				while True:
+					tmp_char = ser.read().decode('utf-8')
+
+					# is closing marker is read, the message is over; otherwise keep reading
+					if tmp_char != PACKET_END_MARKER:
+						read_input += tmp_char
+					else:
+						reading_in_progress = False
+						break;
+
+		# printing to screen for debugging purposes. can be removed when the system has been tested to work well
+		# print(read_input)
+		return read_input
 
 
 	# This function scans all the ports and registers a serial connection when an arduino is found
 	# connected to it. It then uses the serial connection to interact with the arduino using read 
 	# and write instructions.
 	def scan_ports_initialize(self):
-		for i in ports:
-			if ('usb') in i[0]:
+
+		NUMBER_OF_CONNECTED_ARDUINOS = 1
+		ROVER_SERIAL_PORT = '' # TODO: Update to use the correct port # update 2!! no need to update cz the com port is detected automatically
+		ARM_SERIAL_PORT = ''
+		BAUD_RATE = 9600
+		connected_arduinos_found = 0
+
+		for port in ports:
+
+			if ('usb') in port[0]:
 				# opens the serial connection with the port, specifying baudrate & using read and write  
 				# timeout of less than 1/10th of a second.
-				ArduinoUnoSerial = serial.Serial(i[0], 9600, timeout=0.05, write_timeout=0.05)
-				arduino_id_response = ''
-				# if the read times-out, we try again until we get its response.
-				while(not arduino_id_response):
-					ArduinoUnoSerial.write(b'id')
-					arduino_id_response = str(ArduinoUnoSerial.read(5), encoding='ascii')
-				self.book[arduino_id_response] = ArduinoUnoSerial
+				Arduino_Serial = serial.Serial(port[0], 9600, timeout=0.05, write_timeout=0.05)
+				# Arduino_Serial = serial.Serial(port.device, BAUD_RATE, timeout = 1)
+
+
+				# arduino_id_response = ''
+				# # if the read times-out, we try again until we get its response.
+				# while(not arduino_id_response):
+				# 	Arduino_Serial.write(b'id')
+				# 	arduino_id_response = str(Arduino_Serial.read(5), encoding='ascii')
+				# a simple message to the arduino signalling a response has been received
+				Arduino_Serial.write('id'.encode())
+
+				# while loop to make sure the arduino has received the message by received a "done!" string
+				arduino_id_response = self.read_from_serial(Arduino_Serial, BAUD_RATE)
+
+				# if read from serial did not timeout:
+				if(len(arduino_id_response) > 0):
+					# increment the number of arduinos connected
+					secure_connection = True
+					connected_arduinos_found += 1
+
+					# figure which arduino is connected depending on the message sent by the arduino through serial
+					if arduino_id_response == "Motor driver":
+						print("Motor driver is connected!")
+
+
+					if arduino_id_response == "Robot arm":
+						print("Robot arm is connected!")
+
+				else:
+					print(f"unsuccessful connection with {port_COM}")
+
+				self.COM_ports[arduino_id_response] = Arduino_Serial
 
 
 
@@ -49,7 +111,7 @@ def main():
 	asf = Arduino_serial_finder()
 	asf.scan_ports_initialize()
 	# get the serial based on the arduino's ID.
-	connection = asf.get_serial('Nano')
+	connection = asf.get_serial_port('Nano')
 	if(connection):
 		print(connection)
 	else:
---
+++ joystick_demo.py
@@ -0,0 +1,109 @@
+# joystick_demo.py
+# This program will read the x and y axis data from a USB connected joystick and
+# will convert that data into left/right speed values for the runt rover. Then it
+# will send those 2 values to the runt rover over serial in the form: 
+#
+# <start marker><left speed integer><seperator><right speed integer><end marker>
+#
+# The left and right speeds are values [-100, 100] where a negative value
+# specifies the reverse direction. The start marker is the '>' character. The
+# seperator is a comma. The end marker is the '<' character.
+
+import pygame
+import serial
+from arduino_ports_init import find_COM_ports
+from time import time
+
+MOTOR_DRIVE_SERIAL_PORT = '' # TODO: Update to use the correct port
+BAUD_RATE = 9600
+PACKAGE_SIZE = 9
+
+MAX_VAL = 100
+X_AXIS_DEADZONE = 0.14 * MAX_VAL
+Y_AXIS_DEADZONE = 0.14 * MAX_VAL
+Z_AXIS_DEADZONE = 0.20 * MAX_VAL # Rotation around z axis
+SLIDER_OFFSET = 1 * MAX_VAL # Slider in front of joystick
+
+class joystick_demo:
+
+    def control_runt_rover(self):
+        # detect the available ports and store them in a list
+        (MOTOR_DRIVE_SERIAL_PORT, arm_port) = find_COM_ports()
+
+        pygame.init()
+        try:
+            ser = serial.Serial(MOTOR_DRIVE_SERIAL_PORT, BAUD_RATE)
+
+        except serial.SerialException:
+            print("Cannot find motor drive arduino.")
+            return
+
+        current_time = time()
+
+        # Initialize joystick
+        try:
+            joystick = pygame.joystick.Joystick(0)
+            joystick.init()
+        except pygame.error:
+            print("Cannot find joystick. Not running joystick.")
+            return
+
+        while pygame.joystick.get_count() > 0:
+            if (time() - current_time) < 0.05:
+                continue
+            current_time = time()
+
+            # Retrieve joystick data
+            # X and Y axis is range [-100.0, 100.0] where negative is reverse
+            pygame.event.get()
+            x_axis = joystick.get_axis(0) * MAX_VAL
+            y_axis = (0 - joystick.get_axis(1)) * MAX_VAL
+
+            # Rotation around Z axis is range [-100.0, 100.0] where negative is left rotation
+            z_axis = joystick.get_axis(3) * MAX_VAL
+            
+            # Limiter is in range [0, 10.0] where 0 is when slider is all the way
+            # back towards negative sign
+            limiter = (0 - joystick.get_axis(2)) * MAX_VAL
+            limiter = (limiter + SLIDER_OFFSET) / 2 # Maps from [-100.0, 100.0] to [0, 100.0]
+
+            # Set initial speed before considering turning
+            speedLeft = speedRight = y_axis
+
+            if abs(x_axis) < X_AXIS_DEADZONE and abs(y_axis) < Y_AXIS_DEADZONE:
+                # Joystick is centred
+                if abs(z_axis) > Z_AXIS_DEADZONE:
+                    # Rotate rover in place
+                    speedLeft += z_axis
+                    speedRight -= z_axis
+            else:
+                # Joystick is not centred
+                if x_axis != 0:
+                    # Turn rover
+                    speedLeft += x_axis
+                    speedRight -= x_axis
+
+            # Limit values
+            speedLeft = -limiter if speedLeft < -limiter else limiter if speedLeft > limiter else speedLeft
+            speedRight = -limiter if speedRight < -limiter else limiter if speedRight > limiter else speedRight
+
+            # Arduino expects ints
+            speedLeft = int(speedLeft)
+            speedRight = int(speedRight)
+
+            # :<{PACKAGE_SIZE} will make sure packet has a length of PACKAGE_SIZE
+            # packet = f'{(">" + str(speedLeft) + "," + str(speedRight) + "<"):<{PACKAGE_SIZE}}'
+            packet = ">" + str(speedLeft) + "," + str(speedRight) + "<"
+            print(packet)
+            ser.write(packet.encode())
+
+        	
+def main():
+
+    try:
+        joystick_demo().control_runt_rover()
+    except:
+        print("The arduino has been disconnected")
+
+if __name__ == '__main__':
+    main()
