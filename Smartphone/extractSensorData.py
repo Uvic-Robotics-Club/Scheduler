56341e6f39f9f03a2812d1338fdc2aa3eba7bd66
James Tang <jamestang@uvic.ca>
2020-08-27 15:35:35 -0700

    Fixed formatting issues

--- Smartphone/extractSensorData.py
+++ Smartphone/extractSensorData.py
@@ -2,37 +2,40 @@ import socket
 import math
 import traceback
 
-valuesPerSensor = 3
+VALUES_PER_SENSOR = 3
 
 
 def extract_SensorData_v2(data_string):
-		'''Extracts sensors'data from the input raw data string.
-		The return object is an array of arrays [i][j], where i corresponds to sampled acquisitions and j corresponds to sensors' value.
-		All sensors' values are represented as strings.
+	'''Extracts sensors'data from the input raw data string.
+	The return object is an array of arrays [i][j], where i corresponds to sampled acquisitions and j corresponds to sensors' value.
+	All sensors' values are represented as strings.
 
-		Variable:
-		packages - an unformatted list of strings containing all sensor's values
-		packVal - a list of 3 strings (or 1 if its timestamp) containing sensor's xyz's values
-		retVal - a list of packVals
-		'''
-		packages = data_string.split('\r\n')
-		retVal = []
-		for pack in packages:
-			if pack!='' :
-				try:
-					packVal = []
-					packSplit = pack.replace('\n', '').replace('\r', '').split(",")
-					numSensors = int(math.floor( len(packSplit)  / valuesPerSensor))
-					# Checks if packSplit doesn't contain the sensor_name obtained from phone
-					if(packSplit[0].isdigit() and len(packSplit) != 1):
-						p = [packSplit[0]]
+	This function replaces the __extractSensorData within HIMUServer.py because of a bug where it would 
+	mix different sensor_values from different sensors and get rid of the trailing sensor_data
+
+	Variable:
+	packages - an unformatted list of strings containing all sensor's values
+	packVal - a list of 3 strings (or 1 if its timestamp) containing sensor's xyz's values
+	retVal - a list of packVals
+	'''
+	packages = data_string.split('\r\n')
+	retVal = []
+	for pack in packages:
+		if pack != '':
+			try:
+				packVal = []
+				packSplit = pack.replace('\n', '').replace('\r', '').split(",")
+				numSensors = int(math.floor(len(packSplit) / VALUES_PER_SENSOR))
+				# Checks if packSplit doesn't contain the sensor_name obtained from phone
+				if packSplit[0].isdigit() and len(packSplit) != 1:
+					p = [packSplit[0]]
+					packVal.append(p)
+					packSplit = packSplit[1:]
+					for i in range(0, numSensors):
+						p = packSplit[i * VALUES_PER_SENSOR : (i+1) * (VALUES_PER_SENSOR)]
 						packVal.append(p)
-						packSplit = packSplit[1:]
-						for i in range(0 , numSensors):
-							p = packSplit[i*valuesPerSensor : (i+1)*(valuesPerSensor)]
-							packVal.append(p)
-						if(len(packVal)>0):
-							retVal.append(packVal)
-				except Exception as ex:
-					pass
-		return retVal
+					if len(packVal) > 0:
+						retVal.append(packVal)
+			except Exception as ex:
+				pass
+	return retVal
--- Smartphone/phone_client.py
+++ Smartphone/phone_client.py
@@ -1,29 +1,29 @@
 from HIMUServer.HIMUServer import HIMUServer
 import json
 
-
-SENSOR_NAME = ["Timestamp","Accelerometer","Magnetometer","Gyroscope","Gravity","Linear Acceleration","Rotation Vector","GPS"] #for model Huawei P20 Pro
+# For model Huawei P20 Pro
+SENSOR_NAME = ["Timestamp","Accelerometer","Magnetometer","Gyroscope","Gravity","Linear Acceleration","Rotation Vector","GPS"]
 #can create another SENSOR_NAME list for another smartphone
 
 
 class SmartphoneListener:
     def __init__(self,serverInstance):
         self.__server = serverInstance
-    def notify (self, sensorData):
-        self.convertDataSensor(sensorData)
+    def notify(self, sensorData):
+        self.convert_DataSensor(sensorData)
     
     @staticmethod
-    def convertDataSensor(sensor_packets):
-        '''
-        Converts sensor data into JSON Object/String and writes the data to a file
+    def convert_DataSensor(sensor_packets):
+        '''Converts sensor data into JSON Object/String and writes the data to a file
+
+        sensor_packets is a huge packet of multiple sensor_data_lists with each sensor_data_list containing one sensor_set
         '''
         try:
             sensor_dict = {}
-            for sensor_packet_list in sensor_packets: #sensorData is a huge packet of multiple sensor_data_lists with each sensor_data_list containing one sensor_set
+            for sensor_packet_list in sensor_packets:
                 sensor_name_index = 0
-                sensor_name = SENSOR_NAME[sensor_name_index]
-                sensor_dict[sensor_name] = {}
-                for sensor  in sensor_packet_list:
+                sensor_dict["Timestamp"] = {}
+                for sensor in sensor_packet_list:
                     sensor_name = SENSOR_NAME[sensor_name_index]
                     if sensor_name == "Timestamp":
                         sensor_dict["Timestamp"] = {"Timestamp": float(sensor[0])}
@@ -31,15 +31,16 @@ class SmartphoneListener:
                         sensor_dict["GPS"] = {"lat":float(sensor[0]),"long":float(sensor[1]),"alt":float(sensor[2])}
                     else:
                         sensor_dict[sensor_name] = {"x":float(sensor[0]),"y":float(sensor[1]),"z":float(sensor[2])}
-                    sensor_name_index+= 1
-                json_dict = json.dumps(sensor_dict) #indent here provides horizontal indent when writing data to the file
-                json_file = open("Smartphone/phone_sensor_data.json","a")
-                json.dump(json_dict,json_file) #provides horizontal indent when writing json_object to file
+                    sensor_name_index += 1
+                json_dict = json.dumps(sensor_dict)
+                json_file = open("Smartphone/phone_sensor_data9.json","a")
+                json.dump(json_dict, json_file)
                 json_file.write("\n")
         except Exception as err:
             pass
-            # Below statement commented because of errors are smartphone not sending enough sensor_values to console, no effect on performance(anything)
+            # Below statement commented because of errors coming from smartphone not sending enough sensor_values to console, no effect on performance(anything)
             #print(str(err))
+
 #HIMUServer Instance
 myHIMUServer = HIMUServer()
 
